3da3c7c0c772b55facbcf948f4a6cf6673896547
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2021-06-07 16:38:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 7 - Condicionais: \r\n");
+
+            int idadeAna = 11;
+            int quantidadePessoas = 2;
+
+            if (idadeAna >= 18)
+            {
+                Console.WriteLine("Ana possui mais de 18 anos de idade. Pode participar.");
+
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Ana não possui mais de 18 anos, mas está acompanhada. Pode entrar.");
+                }
+                else
+                {
+                    Console.WriteLine("Ana não possui mais de 18 anos de idade. Não pode entrar.");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
